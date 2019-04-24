d269d2f7166cdabb9937b1ae4cf885d52b09a415
sean <987250095@qq.com>
2019-04-24 17:17:55 +0800

    '类，对象，属性，方法，继承'

---
+++ app/src/main/java/com/thinkwithu/www/kotlindemo/demo_class.kt
@@ -0,0 +1,206 @@
+package com.thinkwithu.www.kotlindemo
+
+/**
+ * Copyright (C), 2015-2019, 北京九州申友教育咨询有限公司
+ * Author: sean
+ * Date: 2019/4/23 17:13
+ * Description: ${DESCRIPTION}
+ */
+fun main() {
+
+    /* var f = FourClass(88)
+     f.cc()
+     var fi = Five("", 1, "")*/
+    /* var a = eight.nine(10).foo()
+     println(a)*/
+    var c = C()
+    c.foo()
+
+}
+
+//类用class修饰，
+class FirstClass {
+    public var change: Int = 5 //var申明可变变量
+    val a: String = "4" //val申明常量，相当于final
+
+
+    //成员函数
+    fun foo() {
+        println("foo")
+    }
+
+
+}
+
+class SecondClass constructor(a: String) {
+    //类的主构造器，用constructor修饰，如果没有用可见修饰符修饰，那么constructor可以省略
+    var firstClass = FirstClass()//我们可以像使用普通方法那样对类进行实例化，kt中没有new关键字
+    var b: Int = 4
+
+    //写了主构造器，能写其他构造器就用this代理主构造器
+    /*  constructor(username: String, age: Int){
+      }
+      constructor(b: Int) {
+          this.b = b
+      }*/
+
+
+    fun foo() {
+        firstClass.change//通过类名.来获取属性及方法
+        firstClass.foo()
+    }
+}
+
+class ThreeClass() {
+    var a = 5
+        get() {
+            return field + 2008//field只能在get和set方法中使用，用来代替变量的value
+        }
+        set(value) {
+            field = value
+        }
+    var b = 9
+        get
+        set
+
+    lateinit var c: String//lateinit用来延迟初始化， 不然在声明变量的时候都必须初始化,只能用于变量var
+}
+
+class FourClass(var a: Int) { //主构造器里面申明的变量可以在类范围使用
+
+    init { //类的初始化方法，值的初始化放在init方法里面
+        println("init  a  的值是$a")
+    }
+
+    fun cc() {
+        a = 9
+        println("cc  a  的值 是$a")
+    }
+}
+
+
+class Five(a: Int, c: String) { //主构造函数如果有参数，那么次构造函数也必需接收这些参数，并为主构造函数代理，使用this关键字
+    constructor(b: String, a: Int, c: String) : this(a, c) {
+
+    }
+}
+
+open class animal(a: Int) { //open修饰的类才能被继承，默认是被final修饰
+    open fun foo() { //open修饰的方法才能被重写，默认是final修饰
+
+    }
+}
+
+class person : animal(5) {
+    //继承用:格式,如果父类有传参构造方法，那么子类必须实现它
+    override fun foo() {
+
+    }
+}
+
+
+abstract class six(a: Int) { //abstract修饰抽象类 抽象类不需要用open修饰，也可以被继承
+    abstract fun foo() //abstract修饰抽象方法，也可以不用open修饰
+}
+
+class seven : six(8) {
+    override fun foo() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+class eight() {
+    class nine(a: Int) { //调用嵌套类使用：外部类.嵌套类.属性（方法）
+        fun foo() = 8
+    }
+}
+
+
+class head {
+    var a = 100
+    fun b() = 99
+    inner class foot() { //内部类用inner 修饰，内部类持有外部类的引用，所以能直接访问外部类的属性和方法
+        var c = a
+        fun d() {
+            var e = b()
+        }
+    }
+}
+
+class book : Any() //所有的类都默认继承自any类。
+
+
+open class Person(name: String)
+
+class student(name: String, height: Int) : Person(name) //子类有主构造函数，则基类必须在主构造函数中立即初始化
+
+class teacher : Person {  //子类没有主构造函数，则必须在每一个次构造函数中,用super关键字实例化父类
+    constructor(name: String) : super(name) {
+
+    }
+
+    constructor(height: Int, name: String) : super(name) {
+
+    }
+
+}
+
+
+open class A {
+    open val a = 99 //属性被open修饰，可以被重写
+    open fun foo() {
+        println("A")
+    }
+}
+
+interface B {
+    fun foo() { //接口的方法默认是open
+        println("B")
+    }
+}
+
+class C : A(), B { //子类c继承A和实现B,如果A和B有相同的方法，则子类C必须重写这个方法，我们可以用super<>关键字调用哪个父类的方法
+    override fun foo() {  //kotlin和java一样，也不能多继承
+        super<A>.foo()
+        super<B>.foo()
+    }
+
+    override var a = 100  //val修饰的常量可以被重写成var变量，反之则不行
+    fun d() {
+        a = 999
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/thinkwithu/www/kotlindemo/demo_conditions.kt
+++ app/src/main/java/com/thinkwithu/www/kotlindemo/demo_conditions.kt
@@ -8,10 +8,104 @@ package com.thinkwithu.www.kotlindemo
  */
 
 // main方法是kt的进入方法，好比java的main方法
-fun main(){
+fun main() {
+//    conditionif(5, 9);
+//    conditionwhen("李四", 5)
+//    conditionfor()
+    conditionwhile()
+}
+
+fun conditionif(a: Int, b: Int) {
+    //传统用法
+    var max: Int = b
+    if (a > b) max = a
+    //用if  else
+    if (a > b) {
+        max = a
+    } else {
+        max = b
+    }
+    //if作为表达式，可以进行赋值，取代java的三目运算符
+    max = if (a > b) {
+        a
+    } else {
+        b
+    }
+    max = if (a > b) a else b
+    println("max的值是$max")
+
+    if (a in 5..9) {
+        println("$a 在区间")
+    }
+}
+
+//when 相当于增强版的java switch case语句 支持任意类型，不用写break
+fun conditionwhen(a: String, b: Int) {
+    when (a) {
+        "张三" -> println("我是张三")
+        "李四" -> println("我是李四")
+        else -> println("我是王五")
+    }
+
+    when (b) {
+        4, 5 -> println("b的值是4或者5")//条件可以写在一起，
+        in 1..10 -> println("在区间1到10之间")//支持in  ,!in写法
+        !in 50..100 -> println("不在50到100之间")
+        is Int -> println("是整型")//支持is  !is写法
+    }
+
+    when {
+        b == 1 -> println("b的值是1") //不传参，取代if  else 的写法，不传参数
+        b == 2 -> println("b的值是2")
+        else -> println("b的值不知道是多少")
+    }
+
+    var d = when (b) { //when语句作为表达式，可以进行赋值
+        1 -> 11
+        2 -> 22
+        else -> 8
+    }
+    println(d)
+}
+
+fun conditionfor() {
+    var a = arrayOf(4, 5, 6)
+    for (b in a) {
+        println(b)
+    }
+    for (c in a.withIndex()) {
+        println(c)
+    }
+    for (d in a) {
+        if (d == 4) continue //结束本次循环，继续下一次
+        if (d == 5) break //跳出循环
+    }
+
+    oop@ for (e in a) {  //以名字加上@符号 给表达式取名
+        for (j in a) {
+            break@oop //跳出最外层循环
+        }
+    }
 
 
 }
+
+fun conditionwhile() {
+    var a = 0
+    while (a > 0) { //满足条件 就会继续执行
+        println(--a)
+    }
+    println("--------------")
+    do {
+        println(--a)  //先执行一次，在进去while判断
+    } while (a > 0)
+
+
+}
+
+
+
+
 
 
 
